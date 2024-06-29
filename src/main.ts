1db8e9e6cf51785f6981dd331c93ccf7b03e2d6e
Evgenii Alekseenko <eugene.alexeenko@gmail.com>
2024-06-29 22:00:33 +0200

    feat: setup swagger autogeneration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+function setupSwagger(app: INestApplication) {
+  const SWAGGER_PATH = 'api/swagger';
+
+  const config = new DocumentBuilder()
+    .setTitle('Water Jug Riddle')
+    .setDescription('API description')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(SWAGGER_PATH, app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  setupSwagger(app);
 
   await app.listen(3000);
 }
--- src/water-jug-riddle/water-jug-riddle.controller.ts
+++ src/water-jug-riddle/water-jug-riddle.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { WaterJugRiddleService } from './water-jug-riddle.service';
 import { SolutionRequestDto } from './dto/solution-request.dto';
 import { SolutionResponseDto } from './dto/solution-response.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('water-jug-riddle')
 @Controller('water-jug-riddle')
 export class WaterJugRiddleController {
   constructor(private readonly waterJugRiddleService: WaterJugRiddleService) {}
