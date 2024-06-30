543d6531357555793f90d3f1c1acb0a8f7d7e8e0
Evgenii Alekseenko <eugene.alexeenko@gmail.com>
2024-06-30 09:58:11 +0200

    test: add e2e test for a WaterJugRiddleController

---
+++ test/water-jug-riddle.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('WaterJugRiddleController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    );
+    await app.init();
+  });
+
+  describe('POST /api/water-jug-riddle/solution', () => {
+    it('should return an error in case of empty user input', () => {
+      const expectedResponse = {
+        message: [
+          'jug1Capacity must be a positive number',
+          'jug2Capacity must be a positive number',
+          'targetVolume must be a positive number',
+        ],
+        error: 'Bad Request',
+        statusCode: 400,
+      };
+
+      return request(app.getHttpServer())
+        .post('/api/water-jug-riddle/solution')
+        .expect(400)
+        .expect(expectedResponse);
+    });
+
+    it('should return correct answer for a classic challenge (3 and 5 gallon jugs, target 4)', () => {
+      const expectedResponse = {
+        solution: [
+          { step: 1, jug1Volume: 5, jug2Volume: 0, action: 'Fill Jug1' },
+          {
+            step: 2,
+            jug1Volume: 3,
+            jug2Volume: 2,
+            action: 'Transfer from Jug2 to Jug1',
+          },
+          { step: 3, jug1Volume: 0, jug2Volume: 2, action: 'Empty Jug1' },
+          {
+            step: 4,
+            jug1Volume: 2,
+            jug2Volume: 0,
+            action: 'Transfer from Jug2 to Jug1',
+          },
+          { step: 5, jug1Volume: 2, jug2Volume: 5, action: 'Fill Jug2' },
+          {
+            step: 6,
+            jug1Volume: 3,
+            jug2Volume: 4,
+            action: 'Transfer from Jug2 to Jug1',
+            status: 'solved',
+          },
+        ],
+      };
+
+      return request(app.getHttpServer())
+        .post('/api/water-jug-riddle/solution')
+        .send({ jug1Capacity: 3, jug2Capacity: 5, targetVolume: 4 })
+        .expect(200)
+        .expect(expectedResponse);
+    });
+  });
+});
